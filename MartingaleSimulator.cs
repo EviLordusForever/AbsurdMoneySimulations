a4d5633a335aad4429293dbce648576d5e7878d5
Trigger Devil <devil.mining.999@gmail.com>
2022-09-17 15:56:55 +0300

    martin simulator base was added

---
+++ MartingaleSimulator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace AbsurdMoneySimulations
+{
+	public static class MartingaleSimulator
+	{
+		public static void Simulate()
+		{
+			Thread myThread = new Thread(SimulateThread);
+			myThread.Name = "Martingale";
+			myThread.Start();
+
+			void SimulateThread()
+			{
+				for (int i = 0; i < 999999; i++)
+				{
+					MessageBox.Show($"{i}");
+				}
+			}
+		}
+	}
+}
